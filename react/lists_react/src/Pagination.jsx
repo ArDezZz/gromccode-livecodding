ac1e8acc4e67f5a50844203f7e79291e7ec1486e
ArDezZz <aromudez@gmail.com>
2022-06-09 15:26:42 +0300

    fix

--- react/lists_react/src/Pagination.jsx
+++ react/lists_react/src/Pagination.jsx
@@ -6,11 +6,11 @@ const Pagination = ({ curPage, goNext, goPrev, totalItems, itemsPerPage }) => {
 
   return (
     <div className="pagination">
-      <button disabled={isPrevPageAvailable} onClick={goPrev} className="btn">
+      <button disabled={isPrevPageAvailable ? 'disabled' : null} onClick={goPrev} className="btn">
         {isPrevPageAvailable ? '' : '←'}
       </button>
       <span className="pagination__page">{curPage}</span>
-      <button disabled={isNextPageAvailable} onClick={goNext} className="btn">
+      <button disabled={isNextPageAvailable ? 'disabled' : null} onClick={goNext} className="btn">
         {isNextPageAvailable ? '' : '→'}
       </button>
     </div>
