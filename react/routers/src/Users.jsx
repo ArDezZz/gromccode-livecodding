52f099e5ba697b48d86ab88891b52f1bdd975484
ArDezZz <aromudez@gmail.com>
2022-06-22 15:10:43 +0300

    Livecodding 27

---
+++ react/routers/src/Users.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import User from './User';
+import { Route, Switch, Link } from 'react-router-dom';
+
+const Users = props => {
+  console.log(props);
+  return (
+    <>
+      <div className="page__content">
+        <h1>Users</h1>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/users/github">Github</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users/facebook">Facebook</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/users/:userId" component={User}></Route>
+        </Switch>
+      </div>
+    </>
+  );
+};
+
+export default Users;
